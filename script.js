6946e5c70ad0392618c81ccaa7fbe06996da0fde
codenant <e.g.dallo@gmail.com>
2024-07-16 20:48:23 +0400

    Second Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', function(){
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
 
